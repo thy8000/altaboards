c392d1bbec4cac4eaa7ff20bb6cf0baa15b553cf
Thunay <thunaymoreiradesoares@gmail.com>
2023-08-05 00:37:32 -0300

    📝 Tailwind

--- app/public/wp-content/themes/altaboards/tailwind.config.js
+++ app/public/wp-content/themes/altaboards/tailwind.config.js
@@ -25,6 +25,7 @@ module.exports = {
       pink: colors.pink,
       amber: colors.amber,
       jade: "#04B571",
+      jadeSecondary: "#00BA73",
       jaffa: "#F38D3B",
       midgray: "#5C5C63",
       apple: "#6C9C44",
