23e8829b95f3a482517d0209398357cc91620c38
Thunay <thunaymoreiradesoares@gmail.com>
2023-08-06 18:03:49 -0300

    ✨ Front Page - Services component

---
+++ app/public/wp-content/themes/altaboards/includes/components/front-page/services-title.php
@@ -0,0 +1,35 @@
+<?php
+
+wp_utils_verify_abspath();
+
+?>
+
+<div class="flex flex-col py-16 gap-10">
+    <div class="w-full">
+        <h2 class="text-4xl text-jade font-medium font-roboto w-full my-0 lg:max-w-[600px] lg:text-5xl">
+            <?php esc_html_e('Controle todo o tipo de documentação. O céu é o limite!', 'it9-obrasoft') ?>
+        </h2>
+    </div>
+
+    <div class="flex flex-col gap-10 lg:flex-row lg:gap-20">
+        <div class="w-full lg:w-6/12">
+            <h3 class="text-3xl text-gray-700 font-medium font-roboto my-0 lg:text-start">
+                <?php esc_html_e('Evite surpresas com riscos desnecessários', 'it9-obrasoft') ?>
+            </h3>
+
+            <h4 class="text-base font-normal text-midgray w-full lg:max-w-[450px]">
+                <?php esc_html_e('Quando o fornecedor está com a documentação atrasada sua empresa está exposta a diversos perigos e é coautora de possíveis acidentes e falhas. Evite multas, ações na justiça, autuações, entre outros riscos. Proteja a imagem da sua empresa!', 'it9-obrasoft') ?>
+            </h4>
+        </div>
+
+        <div class="w-full lg:w-6/12">
+            <h3 class="text-3xl text-gray-700 font-medium font-roboto my-0 lg:text-start">
+                <?php esc_html_e('Pague apenas o seu fornecedor “compliance”', 'it9-obrasoft') ?>
+            </h3>
+
+            <h4 class="text-base font-normal text-midgray w-full lg:max-w-[450px]">
+                <?php esc_html_e('Com alertas e farol de visualização você poderá controlar em um único painel se os seus fornecedores estão com a documentação em dia. Fornecedor "compliance" é fornecedor pago!', 'it9-obrasoft') ?>
+            </h4>
+        </div>
+    </div>
+</div>
---
+++ app/public/wp-content/themes/altaboards/includes/components/front-page/services.php
@@ -0,0 +1,13 @@
+<?php
+
+wp_utils_verify_abspath();
+
+?>
+
+<section class="bg-slate-50	">
+    <div class="custom-container">
+        <?php
+        wp_utils_get_component('front-page/services-title');
+        ?>
+    </div>
+</section>
