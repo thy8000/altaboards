87a0617182836080c4379c4fdc04f131b7cda2a6
Thunay <thunaymoreiradesoares@gmail.com>
2023-08-27 15:26:57 -0300

    ✨ Support

---
+++ app/public/wp-content/themes/altaboards/includes/components/front-page/support.php
@@ -0,0 +1,29 @@
+<?php
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+?>
+
+<div class="support">
+    <div class="support-container">
+        <div data-w-id="fbaf7d1b-3ed3-7949-b192-07be982b0d82" class="support-div-image">
+            <img src="<?php echo get_template_directory_uri() ?>/assets/images/front-page-support.svg" width="508" alt="">
+        </div>
+
+        <div data-w-id="fbaf7d1b-3ed3-7949-b192-07be982b0d84" class="support-div-content">
+            <h3 class="premium-title">
+                Equipe de suporte profissional sempre disponível para te ajudar
+            </h3>
+
+            <p class="premium-text">
+                Nossa equipe de consultores está a postos para te atender no melhor tempo possível. Entre em contato conosco via <a href="#" class="premium-text-link">chat</a> ou <a href="mailto:suporte@altaboards.com?subject=Contato%20Via%20Site%20-%20Suporte" class="premium-text-link">e-mail</a>.
+            </p>
+
+            <a href="/contact" class="cases-button w-button">
+                Entrar em contato
+            </a>
+        </div>
+    </div>
+</div>
