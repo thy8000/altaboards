f833afc028e1c9826504e389c51702430a95cba2
Thunay <thunaymoreiradesoares@gmail.com>
2023-09-14 22:27:52 -0300

    🩹 About Us

--- app/public/wp-content/themes/altaboards/pages/page-about-us/_index.php
+++ app/public/wp-content/themes/altaboards/pages/page-about-us/_index.php
@@ -9,12 +9,6 @@ $benefits_list = [
         'image'       => esc_url(get_template_directory_uri() . '/assets/images/graphic-creator-benefit.jpeg'),
         'link'        => esc_url(get_page_link(get_page_by_path('services'))),
     ],
-    [
-        'title'       => esc_html__('A Consultoria ObraSoft', 'it9-obrasoft'),
-        'description' => esc_html__('Contamos com uma equipe altamente qualificada e capacitada para auxiliar sua empresa no processo de melhoria contínua do Sistema de Gestão, combinando conhecimento técnico e tecnológico de forma a oferecer as melhores soluções para os desafios enfrentados no dia a dia.', 'it9-obrasoft'),
-        'image'       => esc_url(get_template_directory_uri() . '/assets/images/time-obrasoft.jpg'),
-        'link'        => esc_url(get_page_link(get_page_by_path('contact'))),
-    ],
 ];
 
 wp_utils_get_component('header/_index');
