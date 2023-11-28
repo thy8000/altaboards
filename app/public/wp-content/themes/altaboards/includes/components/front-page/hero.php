8138b5ca4b9040c0596dcae2f0a239dbdb392a58
Thunay <thunaymoreiradesoares@gmail.com>
2023-11-27 23:28:13 -0300

    💄 Hero

--- app/public/wp-content/themes/altaboards/includes/components/front-page/hero.php
+++ app/public/wp-content/themes/altaboards/includes/components/front-page/hero.php
@@ -1,9 +1,11 @@
 <?php
+
 wp_utils_verify_abspath();
 
 $page_contact_link = get_page_link(get_page_by_path('contact'));
 
 $whatsapp_contact_link = "https://api.whatsapp.com/send?phone=5511970800600&text=Ol%C3%A1%2C%20gostaria%20de%20saber%20mais%20sobre%20o%20Altaboards%20Gest%C3%A3o%20de%20Fornecedores!";
+
 ?>
 
 <div class="hero">
@@ -17,6 +19,6 @@ $whatsapp_contact_link = "https://api.whatsapp.com/send?phone=5511970800600&text
             </div>
         </div>
         
-        <img src="<?php echo get_template_directory_uri() ?>/assets/images/front-page-hero-animation.gif" alt="" class="image-2">
+        <img src="<?php echo get_template_directory_uri() ?>/assets/images/front-page-hero-animation.gif" alt="" class="!w-full lg:!w-1/2 !h-[unset] image-2 aspect-video">
     </div>
 </div>
