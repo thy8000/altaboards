1b251441d92b43a81a5f44e05e6e826878d5d2f8
Thunay <thunaymoreiradesoares@gmail.com>
2023-11-27 23:28:02 -0300

    🩹 WP Forms - Prod ID

--- app/public/wp-content/themes/altaboards/includes/components/page-contact/form.php
+++ app/public/wp-content/themes/altaboards/includes/components/page-contact/form.php
@@ -5,21 +5,21 @@ if (!defined('ABSPATH')) exit;
 ?>
 
 <section class="bg-teal-50">
-    <div class="custom-container">
-        <div class="flex flex-col-reverse py-14 gap-[3.75rem] lg:flex-row">
-            <div class="basis-7/12">
-                <?php echo do_shortcode('[wpforms id="196"]') ?>
-            </div>
+   <div class="custom-container">
+      <div class="flex flex-col-reverse py-14 gap-[3.75rem] lg:flex-row">
+         <div class="basis-7/12">
+            <?php echo do_shortcode('[wpforms id="31"]') ?>
+         </div>
 
-            <div class="basis-5/12 flex flex-col">
-                <h2 class="text-4xl font-normal text-jade font-cabin">
-                    <?php esc_html_e('Envie-nos uma mensagem!', 'it9-obrasoft') ?>
-                </h2>
+         <div class="basis-5/12 flex flex-col">
+            <h2 class="text-4xl font-normal text-jade font-cabin">
+               <?php esc_html_e('Envie-nos uma mensagem!', 'it9-obrasoft') ?>
+            </h2>
 
-                <h3 class="text-base font-normal text-gray-600">
-                    <?php esc_html_e('Quer instalar o nosso aplicativo, conhecer nossas soluções, tirar dúvidas, ou nos dar sua opinião/feedback ? Envie-nos uma mensagem, que iremos lhe responder.', 'it9-obrasoft') ?>
-                </h3>
-            </div>
-        </div>
-    </div>
+            <h3 class="text-base font-normal text-gray-600">
+               <?php esc_html_e('Quer instalar o nosso aplicativo, conhecer nossas soluções, tirar dúvidas, ou nos dar sua opinião/feedback ? Envie-nos uma mensagem, que iremos lhe responder.', 'it9-obrasoft') ?>
+            </h3>
+         </div>
+      </div>
+   </div>
 </section>
